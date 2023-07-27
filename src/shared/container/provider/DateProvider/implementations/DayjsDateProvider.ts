9b7683520ecc2a84e5cbca7d35e7164b14fc6890
ThiagoDanka <thiagodanka@gmail.com>
2023-07-26 22:43:20 -0400

    CV-3-1- Criando o caso de usu

--- src/shared/container/provider/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/provider/DateProvider/implementations/DayjsDateProvider.ts
@@ -37,6 +37,11 @@ dayjs.extend(utc);
        return dayjs().add(days, "days").toDate();
       
     }
+
+
+    addHours(hours: number): Date {
+      return dayjs().add(hours, "hours").toDate();
+    }
    
 
   }
